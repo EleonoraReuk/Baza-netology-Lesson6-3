9edd079576a10280f97bc6de4519ef86be2c6aa9
Eleonora Reuk <106328644+EleonoraReuk@users.noreply.github.com>
2026-05-22 22:24:54 +0700

    Update Rhombus.cpp

--- Rhombus.cpp
+++ Rhombus.cpp
@@ -3,4 +3,4 @@
 Rhombus::Rhombus(double side, double angle_A, double angle_B)
 	: Parallelogram(side, side, angle_A, angle_B) {}
 
-string Rhombus::get_name() const { return "Ромб"; }
+string Rhombus::get_name() const { return "Р РѕРјР±"; }
